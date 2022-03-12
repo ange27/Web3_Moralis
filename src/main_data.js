138209cafa55f9316518cae9522d45240228e421
ange27 <33702096+ange27@users.noreply.github.com>
2022-03-12 12:22:14 -0500

    Update main_data.js

--- src/main_data.js
+++ src/main_data.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAjj318OH-8l4SdxcKq30pARKsQack8xT4",
-  authDomain: "fictionbooks.firebaseapp.com",
-  databaseURL: "https://fictionbooks-default-rtdb.firebaseio.com",
-  projectId: "fictionbooks",
-  storageBucket: "fictionbooks.appspot.com",
-  messagingSenderId: "396455250657",
-  appId: "1:396455250657:web:8aaf376ee77a90149a45a0",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 const app_data = initializeApp(firebaseConfig);
 export default app_data;
